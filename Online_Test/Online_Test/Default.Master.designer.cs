8eff0a8286a2f091421484a813c5bd96274d0238
Ahmet ÇÜMEN <ahmetcumen@gmail.com>
2016-11-23 16:02:52 +0200

    Master Page duzenlemesi yapildi.

--- Online_Test/Online_Test/Default.Master.designer.cs
+++ Online_Test/Online_Test/Default.Master.designer.cs
@@ -29,5 +29,14 @@ namespace Online_Test {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        
+        /// <summary>
+        /// ScriptSection control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ScriptSection;
     }
 }
