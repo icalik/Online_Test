30b885751f36b1ae80075764f6f6ed03f10b7463
Ahmet ÇÜMEN <ahmetcumen@gmail.com>
2016-12-06 23:51:25 +0200

    Çıkış Yap butonu eklendi, Çıkış yap sayfası eklendi, Session lar silindi.

---
+++ Online_Test/Online_Test/LogOut.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Online_Test
+{
+    public partial class LogOut : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Session.RemoveAll();
+
+            Response.AddHeader("refresh","2;URL=Default.aspx");
+        }
+    }
+}
