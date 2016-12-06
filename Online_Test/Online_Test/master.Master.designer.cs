06d714d0761462755a8386345ba8cf416752e672
Ahmet ÇÜMEN <ahmetcumen@gmail.com>
2016-12-06 21:17:41 +0200

    Master page arayüz düzeltildi

--- Online_Test/Online_Test/master.Master.designer.cs
+++ Online_Test/Online_Test/master.Master.designer.cs
@@ -38,5 +38,14 @@ namespace Online_Test {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        
+        /// <summary>
+        /// ContentPlaceHolder2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder2;
     }
 }
--- Online_Test/Online_Test/test.aspx
+++ Online_Test/Online_Test/test.aspx
@@ -1,10 +1,5 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/master.Master" AutoEventWireup="true" CodeBehind="test.aspx.cs" Inherits="Online_Test.test" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
-
-
-
-
- 
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
     
