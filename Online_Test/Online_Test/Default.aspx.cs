6ff483a80c59c515c7544d78a657245322e72a84
İsmet Said ÇALIK <ismetsaid.calik@gmail.com>
2016-12-17 18:30:18 +0300

    Ana sayfa konular kismi yapildi.

--- Online_Test/Online_Test/Default.aspx.cs
+++ Online_Test/Online_Test/Default.aspx.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.UI;
 using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
@@ -12,7 +14,40 @@ namespace Online_Test
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string baglanti = WebConfigurationManager.ConnectionStrings["OnlineTestConnectionString"].ConnectionString;
+            SqlConnection con = new SqlConnection(baglanti);
+            con.Open();
+            if (con.State == System.Data.ConnectionState.Open)
+            {
+                try
+                {
+                   
+                    string q = "SELECT TOP 100 [konu_id],[konu_adi],[ekleyen] FROM[dbo].[Konular] order by konu_adi asc";
 
+                    SqlCommand com = new SqlCommand(q, con);
+                    SqlDataReader dr = com.ExecuteReader();
+
+                    while (dr.Read())
+                    {
+                        //konu_goster.Text = Convert.ToString(dr["konu_adi"]);
+                        
+                       Literal1.Text =  Literal1.Text +  dr["konu_adi"] + "<br/>";
+
+
+
+                    }
+                    dr.Close();
+                }
+                catch (Exception)
+                {
+                    
+
+                }
+
+
+
+
+            }
         }
 
         protected void btn_gir_Click(object sender, EventArgs e)
