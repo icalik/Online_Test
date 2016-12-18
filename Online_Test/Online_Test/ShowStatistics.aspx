ebad282782a1e098d8354de0fbb58eae6a28007d
Ahmet ÇÜMEN <ahmetcumen@gmail.com>
2016-12-18 04:58:37 +0200

    İstatistik kısmı eklendi

--- Online_Test/Online_Test/ShowStatistics.aspx
+++ Online_Test/Online_Test/ShowStatistics.aspx
@@ -3,8 +3,11 @@
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
 
+    
+<asp:PlaceHolder ID = "PlaceHolder1" runat="server" />
 
 
-</asp:Content>
+
+    </asp:Content>
 <asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
 </asp:Content>
--- Online_Test/Online_Test/ShowStatistics.aspx.cs
+++ Online_Test/Online_Test/ShowStatistics.aspx.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Text;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -9,9 +13,96 @@ namespace Online_Test
 {
     public partial class ShowStatistics : System.Web.UI.Page
     {
+        string id;
         protected void Page_Load(object sender, EventArgs e)
         {
+            /*
+            id = Session["id"].ToString();
+            // string ad_gelen = Convert.ToString(txt_profil_ad.Text);
+            string baglanti = WebConfigurationManager.ConnectionStrings["OnlineTestConnectionString"].ConnectionString;
+            SqlConnection con = new SqlConnection(baglanti);
+            con.Open();
+            if (con.State == System.Data.ConnectionState.Open)
+            {
+                string q = "select ct.test_id,t.test_adi,k.konu_adi,t.soru_sayisi ,count(durum) as dogru_sayisi from Cozulen_Test ct,Testler t, Konular k where ct.cozen_id = '"+ id +"' and ct.test_id = t.test_id and t.konu_id = k.konu_id and durum = 1 group by ct.test_id,t.test_adi,k.konu_adi,t.soru_sayisi; ";
+                SqlCommand com = new SqlCommand(q, con);
+
+                SqlDataReader dr = com.ExecuteReader();
+                while (dr.Read())
+                {
+
+                    
+                }
+
+            }
+            */
+            if (!this.IsPostBack)
+            {
+                //Populating a DataTable from database.
+                DataTable dt = this.GetData();
+
+                //Building an HTML string.
+                StringBuilder html = new StringBuilder();
+
+                //Table start.
+                html.Append("<table border = '1'>");
+
+                //Building the Header row.
+                html.Append("<tr>");
+                foreach (DataColumn column in dt.Columns)
+                {
+                    html.Append("<th>");
+                    html.Append(column.ColumnName);
+                    html.Append("</th>");
+                }
+                html.Append("</tr>");
+
+                //Building the Data rows.
+                foreach (DataRow row in dt.Rows)
+                {
+                    html.Append("<tr>");
+                    foreach (DataColumn column in dt.Columns)
+                    {
+                        html.Append("<td>");
+                        html.Append(row[column.ColumnName]);
+                        html.Append("</td>");
+                    }
+                    html.Append("</tr>");
+                }
+
+                //Table end.
+                html.Append("</table>");
+
+                //Append the HTML string to Placeholder.
+                PlaceHolder1.Controls.Add(new Literal { Text = html.ToString() });
+            }
+
 
         }
+        private DataTable GetData()
+        {
+            string baglanti = WebConfigurationManager.ConnectionStrings["OnlineTestConnectionString"].ConnectionString;
+           
+            using (SqlConnection con = new SqlConnection(baglanti))
+            {
+                using (SqlCommand cmd = new SqlCommand("select ct.test_id,t.test_adi,k.konu_adi,t.soru_sayisi ,count(durum) as dogru_sayisi from Cozulen_Test ct,Testler t, Konular k where ct.cozen_id = '" + id + "' and ct.test_id = t.test_id and t.konu_id = k.konu_id and durum = 1 group by ct.test_id,t.test_adi,k.konu_adi,t.soru_sayisi; "))
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter())
+                    {
+                        cmd.Connection = con;
+                        sda.SelectCommand = cmd;
+                        using (DataTable dt = new DataTable())
+                        {
+                            sda.Fill(dt);
+                            return dt;
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+
     }
 }
